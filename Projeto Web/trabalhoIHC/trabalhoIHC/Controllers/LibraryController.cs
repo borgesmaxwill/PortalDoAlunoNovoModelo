0612789d784bc6c2af5a573669dbb3bddb078ed9
Max Willian <maxwillianob@hotmail.com>
2017-06-02 12:19:34 -0400

    Adicionando html inicial das buscas de livros

---
+++ Projeto Web/trabalhoIHC/trabalhoIHC/Controllers/LibraryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace trabalhoIHC.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public ActionResult QuickSearch()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult QuickSearch()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult DetailedSearch()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult DetailedSearch()
+        //{
+        //    return View();
+        //}
+    }
+}
