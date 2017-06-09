6ba63cfdcb110ae097eb576d877d7e4d4db99e1f
THAIS-PC\thais <thais.cristina@infouniron.com>
2017-06-08 20:50:24 -0400

    Modificação do Projeto Atual.

---
+++ Projeto Web/trabalhoIHC/trabalhoIHC/Controllers/SecretaryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace trabalhoIHC.Controllers
+{
+    public class SecretaryController : Controller
+    {
+        public PartialViewResult RegistrationStatement()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SchoolRecords()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult RequestsTable()
+        {
+            return PartialView();
+        }
+    }
+}
